dff5da7c5b9f2723aa72b986f844cffd4caf18fe
NQuinn27 <niall.quinn.27@gmail.com>
2016-10-21 11:26:31 +0100

    Added JSON support for both endpoints using Jackson

--- Temperature-Converter/src/main/java/com/groupg/temperature/converter/FarToCelService.java
+++ Temperature-Converter/src/main/java/com/groupg/temperature/converter/FarToCelService.java
@@ -6,10 +6,17 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+
 /*
  * Copyright 2016 Niall Quinn.
  */
-
 /**
  *
  * @author Niall Quinn
@@ -17,10 +24,11 @@ import javax.ws.rs.core.Response;
 @Path("/far2cel")
 
 public class FarToCelService {
+
     @GET
     @Path("/{param}")
     @Produces(MediaType.TEXT_HTML)
-    public Response convertFahrenheitToCelcius(@PathParam("param") String message) {
+    public Response convertFahrenheitToCelciusHTML(@PathParam("param") String message) {
         TemperatureConverter con = new TemperatureConverter();
         System.out.print("Here");
         Double val = Double.parseDouble(message);
@@ -28,5 +36,32 @@ public class FarToCelService {
         String resString = String.format("%.2f", res);
         String output = "<html><body><h1>" + message + " fahrenheit is " + resString + " celcius</h1></body></html>";
         return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response convertFahrenheitToCelciusJSON(@PathParam("param") String message) {
+        TemperatureConverter con = new TemperatureConverter();
+        Double val = Double.parseDouble(message);
+        double res = con.convertFarToCel(val);
+        String resString = String.format("%.2f", res);
+
+        try {
+            JsonFactory factory = new JsonFactory();
+            Writer writer = new StringWriter();
+            JsonGenerator g = factory.createGenerator(writer);
+            g.writeStartObject();                           //{
+            g.writeObjectFieldStart("data");                //data : {
+            g.writeStringField("inputValue", message);      //"inputValue" : inValue,
+            g.writeStringField("inputUnit", "Fahrenheit");  //"inputUnit"  : "Fahrenheit",
+            g.writeStringField("outputValue", resString);   //"outputValue": outValue,
+            g.writeStringField("outputUnit", "Celcius");    //"outputUnit" : Celcius
+            g.writeEndObject();                             // }
+            g.close();                                      // }
+            return Response.status(200).entity(writer.toString()).build();
+        } catch (IOException e) {
+            return Response.status(500).entity("Internal Server Error").build();
+        }
     }
 }

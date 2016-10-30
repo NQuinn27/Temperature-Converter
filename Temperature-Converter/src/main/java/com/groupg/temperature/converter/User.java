8ecbc2bb9ad3cf11b4fea8d227c84aede269ae5c
NQuinn27 <niall.quinn.27@gmail.com>
2016-10-30 15:35:09 +0000

    remove unused imports

--- Temperature-Converter/src/main/java/com/groupg/temperature/converter/User.java
+++ Temperature-Converter/src/main/java/com/groupg/temperature/converter/User.java
@@ -5,11 +5,6 @@ package com.groupg.temperature.converter;
 
 import java.io.Serializable;
 import java.util.UUID;
-import javax.persistence.Id;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
